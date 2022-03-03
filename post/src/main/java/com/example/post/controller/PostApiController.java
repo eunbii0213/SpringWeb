facf015d9939d860d83e317b583e757ffe258c0b
eunbii0213 <eunbii0213@naver.com>
2022-03-03 10:26:03 +0900

    post api 사용 실습코드입니다.

---
+++ post/src/main/java/com/example/post/controller/PostApiController.java
@@ -0,0 +1,28 @@
+package com.example.post.controller;
+
+import com.example.post.dto.PostRequestDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class PostApiController {
+    @PostMapping("/post")
+    //value값은 booelan도 있고 number도 있으니까 object형태로 받음
+    public void post(@RequestBody Map<String, Object> requestData){
+        requestData.forEach((key, value) -> {
+            System.out.println("key : " + key);
+            System.out.println("value : " + value);
+        });
+    }
+
+    @PostMapping("/post02")
+    public String post(@RequestBody PostRequestDto dto){
+        System.out.println(dto.toString());
+        return dto.toString();
+    }
+}
